87a7fba4adbd51e9f9d5b9512f94ccc1620d38fc
Bob Zurad <bob@zurad.net>
2025-09-28 16:04:40 -0500

    parse input

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 require("sjcl");
 
-/* TODO:
-  read backup file
-  parse backup file
-  write decrypted notes to output file
-*/
+var fs = require("fs");
+var encryptedNotes;
 
-console.log("hello world");
+var note = {
+  content: "",
+  dateCreated: new Date(),
+  dateUpdated: new Date(),
+  isEncrypted: false,
+  title: "",
+};
+var noteId = "";
+
+// read file
+fs.readFile("input/encrypted.json", callback);
+
+// callback for reading file
+function callback(err, data) {
+  if (err) throw err;
+  // parse JSON with reviver
+  encryptedNotes = JSON.parse(data, (key, value) => {
+    // get nodeId
+    if (key.startsWith("-")) {
+      noteId = key;
+    }
+
+    // build note object
+    note[key] = value;
+
+    // get authId
+    if (
+      key !== "notes" &&
+      key !== "content" &&
+      key !== "dateCreated" &&
+      key !== "dateUpdated" &&
+      key !== "isEncrypted" &&
+      key !== "title" &&
+      !key.startsWith("-")
+    ) {
+      // key must be the authId
+      decryptNote(note, key);
+    }
+  });
+}
+
+function decryptNote(note, key) {
+  console.log(key);
+}
