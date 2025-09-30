1c1eebf5f8f79bdc4132b353ee1ba540026d99e0
Bob Zurad <bob@zurad.net>
2025-09-30 15:08:17 -0500

    more testing

--- src/decrypter.js
+++ src/decrypter.js
@@ -1,75 +1,83 @@
 import * as fs from "fs";
 import sjcl from "sjcl";
 
-var note = {
-  content: "",
-  dateCreated: new Date(),
-  dateUpdated: new Date(),
-  isEncrypted: false,
-  title: "",
-};
-var noteId = "";
+export async function decrypter(path) {
+  // note obj
+  var note = {
+    content: "",
+    dateCreated: new Date(),
+    dateUpdated: new Date(),
+    isEncrypted: false,
+    title: "",
+  };
 
-export function decrypter(path) {
+  var currentNoteId = "";
+  var noteIds = [];
+
   // read file
-  fs.readFile(path, callback);
+  //fs.readFile(path, callback);
+  const data = fs.readFileSync(path, { encoding: "utf8", flag: "r" });
 
-  // callback for reading file
-  function callback(err, data) {
-    if (err) throw err;
+  // // callback for reading file
+  // async function callback(err, data) {
+  //   if (err) throw err;
 
-    // parse JSON with reviver
-    var encryptedNotes = JSON.parse(data, (key, value) => {
-      // get noteId
-      if (key.startsWith("-")) {
-        noteId = key;
+  // parse JSON with reviver
+  var encryptedNotes = await JSON.parse(data, (key, value) => {
+    // get currentNoteId
+    if (key.startsWith("-")) {
+      currentNoteId = key;
+      noteIds.push(key);
+    }
+
+    // build note object
+    note[key] = value;
+
+    // TODO: build a buffer of sibling notes before decrypting (because we don't have key yet)
+
+    // get authId
+    if (
+      key !== "notes" &&
+      key !== "content" &&
+      key !== "dateCreated" &&
+      key !== "dateUpdated" &&
+      key !== "isEncrypted" &&
+      key !== "title" &&
+      !key.startsWith("-")
+    ) {
+      // we now have the entire note
+      if (note.isEncrypted === true) {
+        // key must be the authId
+        decryptNote(key, currentNoteId, note);
+      } else {
+        console.log(currentNoteId);
+        console.log(note.title);
+        console.log(note.content);
       }
+      // clear note object
+      note = {
+        content: "",
+        dateCreated: new Date(),
+        dateUpdated: new Date(),
+        isEncrypted: false,
+        title: "",
+      };
+    }
+  });
 
-      // build note object
-      note[key] = value;
-
-      // TODO: build a buffer of sibling notes before decrypting (because we don't have key yet)
-
-      // get authId
-      if (
-        key !== "notes" &&
-        key !== "content" &&
-        key !== "dateCreated" &&
-        key !== "dateUpdated" &&
-        key !== "isEncrypted" &&
-        key !== "title" &&
-        !key.startsWith("-")
-      ) {
-        // we now have the entire note
-        if (note.isEncrypted === true) {
-          // key must be the authId
-          decryptNote(key, noteId, note);
-        } else {
-          console.log(noteId);
-          console.log(note.title);
-          console.log(note.content);
-        }
-        // clear note object
-        note = {
-          content: "",
-          dateCreated: new Date(),
-          dateUpdated: new Date(),
-          isEncrypted: false,
-          title: "",
-        };
-      }
-    });
-  }
-
-  // decrypts the note with the given key
-  function decryptNote(key, noteId, note) {
-    var title = sjcl.decrypt(key, note.title);
-    var content = sjcl.decrypt(key, note.content);
-    console.log(noteId);
-    console.log(title);
-    console.log(content);
-  }
+  return { noteIds };
 }
+
+// decrypts the note with the given key
+function decryptNote(key, currentNoteId, note) {
+  var title = sjcl.decrypt(key, note.title);
+  var content = sjcl.decrypt(key, note.content);
+  console.log(currentNoteId);
+  console.log(title);
+  console.log(content);
+}
+
+
 
 export function sum(a, b) {
   return a + b;
--- src/decrypter.test.js
+++ src/decrypter.test.js
@@ -1,10 +1,12 @@
-import { expect, test } from 'vitest'
+import { expect, expectTypeOf, test } from "vitest";
 import { decrypter, sum } from "./decrypter.js";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("decrypts notes", () => {
-  expect(decrypter("./input/encrypted.test.json"));
+test("decrypts notes", async () => {
+  var subject = await decrypter("./input/encrypted.test.json");
+  expectTypeOf(subject).toBeObject();
+  expect(subject.noteIds.length).toBe(6);
 });
