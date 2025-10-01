9d1ef3ac82d145f3c47a6eea7b65aad8b71d71b7
Bob Zurad <bob@zurad.net>
2025-09-30 20:23:10 -0500

    tests pass

--- src/decrypter.js
+++ src/decrypter.js
@@ -14,52 +14,18 @@ var currentNoteId = "";
 var noteIds = [];
 var notes = [];
 
-export function parse(path) {}
-
-export function decrypt(notes) {}
-
-export async function decrypter(path) {
-  // read file
-  //fs.readFile(path, callback);
+export function parse(path) {
   const data = fs.readFileSync(path, { encoding: "utf8", flag: "r" });
 
-  // // callback for reading file
-  // async function callback(err, data) {
-  //   if (err) throw err;
-
   // parse JSON with reviver
-  var encryptedNotes = await JSON.parse(data, (key, value) => {
+  var encryptedNotes = JSON.parse(data, (key, value) => {
+    //
     // get currentNoteId
     if (key.startsWith("-")) {
       currentNoteId = key;
       noteIds.push(key);
       notes.push(note);
-    }
 
-    // build note object
-    note[key] = value;
-
-    // TODO: build a buffer of sibling notes before decrypting (because we don't have key yet)
-
-    // get authId
-    if (
-      key !== "notes" &&
-      key !== "content" &&
-      key !== "dateCreated" &&
-      key !== "dateUpdated" &&
-      key !== "isEncrypted" &&
-      key !== "title" &&
-      !key.startsWith("-")
-    ) {
-      // we now have the entire note
-      if (note.isEncrypted === true) {
-        // key must be the authId
-        decryptNote(key, currentNoteId, note);
-      } else {
-        console.log(currentNoteId);
-        console.log(note.title);
-        console.log(note.content);
-      }
       // clear note object
       note = {
         content: "",
@@ -69,8 +35,41 @@ export async function decrypter(path) {
         title: "",
       };
     }
+    // build note object
+    note[key] = value;
   });
+  return { noteIds, notes };
+}
 
+export function decrypt(notes) {
+  // get authId
+  if (
+    key !== "notes" &&
+    key !== "content" &&
+    key !== "dateCreated" &&
+    key !== "dateUpdated" &&
+    key !== "isEncrypted" &&
+    key !== "title" &&
+    !key.startsWith("-")
+  ) {
+    // we now have the entire note
+    if (note.isEncrypted === true) {
+      // key must be the authId
+      decryptNote(key, currentNoteId, note);
+    } else {
+      console.log(currentNoteId);
+      console.log(note.title);
+      console.log(note.content);
+    }
+    // clear note object
+    note = {
+      content: "",
+      dateCreated: new Date(),
+      dateUpdated: new Date(),
+      isEncrypted: false,
+      title: "",
+    };
+  }
   return { noteIds, notes };
 }
 
@@ -81,8 +80,4 @@ function decryptNote(key, currentNoteId, note) {
   console.log(currentNoteId);
   console.log(title);
   console.log(content);
-}
-
-export function sum(a, b) {
-  return a + b;
 }
--- src/decrypter.test.js
+++ src/decrypter.test.js
@@ -1,13 +1,27 @@
 import { expect, expectTypeOf, test } from "vitest";
-import { decrypter, sum } from "./decrypter.js";
+import { parse, decrypt, sum } from "./decrypter.js";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test("decrypts notes", async () => {
-  var subject = await decrypter("./input/encrypted.test.json");
+test("parses notes", () => {
+  var subject = parse("./input/encrypted.test.json");
   expectTypeOf(subject).toBeObject();
   expect(subject.noteIds.length).toBe(10);
   expect(subject.notes.length).toBe(10);
+  expect(subject.notes[0].title).toBe(
+    '{"iv":"vWgbQ/O7EP9nGDCFSN3n9g==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"rWKXtaliTkI=","ct":"uOFo2BqtSuZMarMw+Vw9syb67XH0WFcJ"}'
+  );
+  expect(subject.notes[1].title).toBe(
+    '{"iv":"V0eCFTo2wN3m0FrvQb+cuA==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"/JIe1Hi0kXw=","ct":"op3zZu9INIBMdNM/Bw=="}'
+  );
+  expect(subject.notes[2].title).toBe(
+    '{"iv":"DsoJYHtg6REeDtsJm6UHgQ==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"/JIe1Hi0kXw=","ct":"7mGrxqsysKLqVJXpH0rqQ52JwlYXbdR0t82VoKA="}'
+  );
+  expect(subject.notes[3].title).toBe(
+    '{"iv":"m9xlghiMz2VxabKimJTUvQ==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"/JIe1Hi0kXw=","ct":"HK4K6DGKB8mvSp3FMTOjxTqi7jc="}'
+  );
+  expect(subject.notes[4].title).toBe("Your First Note!");
+  expect(subject.notes[5].title).toBe("Bob");
+  expect(subject.notes[6].title).toBe("Bob222");
+  expect(subject.notes[7].title).toBe("First Note!");
+  expect(subject.notes[8].title).toBe("Second Note!");
+  expect(subject.notes[9].title).toBe("SAMUEL L. IPSUM");
 });
